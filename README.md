01471eea110ffd4b78599634ae9a95b2e9e8f634
umesh1201 <reachout2umesh@gmail.com>
2025-02-17 20:05:58 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# umeshchandra.github.io
-profile
+# umechand.github.io
---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+function openModal(modalId) {
+  document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = "none";
+}
